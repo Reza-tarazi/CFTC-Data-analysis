fb1c93d6bb35122749b7e1d22d59dc87227173c5
Reza Tarazi <Rezatarazi21@gmail.com>
2024-11-01 16:20:03 +0330

    CFTC-Data

---
+++ CFTC-DATA.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import datetime
+import time
+import telegram
+import asyncio
+from bs4 import BeautifulSoup
+import requests
+import telegram
+import asyncio
+#====================
+
+browser = webdriver.Firefox()
+browser.get('https://www.cftc.gov/dea/options/financial_lof.htm')
+time.sleep(2)
+
+
+s = str(browser.find_element(By.XPATH , '/html/body/pre').text)
+lines = s.split("\n")
+lines = [line for line in lines if line.strip()]
+PAIR = []
+for i in range(1, len(lines)-1, 17):
+    chunk = "\n".join(lines[i:i+17])
+    PAIR.append(chunk)
+#=========================================================
+#we need to make_dictionary with this data at another function
+'''
+cot_info = {}
+for i in PAIR:
+    make_dictionary(i,cot_info)
+'''
+
+print(lines)
+print(PAIR)
+print("OK remove")
