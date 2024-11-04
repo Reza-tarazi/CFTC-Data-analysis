f09c7e9c47f46c5c28abb0bc1894a19b4f6aaa63
Reza-tarazi <142679889+Reza-tarazi@users.noreply.github.com>
2024-11-04 13:18:54 +0330

    Update CFTC-DATA.py
    This is the final version of the data collection and surface analysis filtering data for the Chicago options and futures market.
    By using these data and using Pandas, you can make the necessary changes according to your taste.

--- CFTC-DATA.py
+++ CFTC-DATA.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import datetime
 import time
-import asyncio
+import pandas as pd
 #====================
+'''
+In the previous editions,
+I was looking to create a dictionary to divide the data,
+so I decided to change the edition with the help of Pandas.
+'''
 
 browser = webdriver.Firefox()
 browser.get('https://www.cftc.gov/dea/options/financial_lof.htm')
@@ -14,31 +18,109 @@ time.sleep(2)
 s = str(browser.find_element(By.XPATH , '/html/body/pre').text)
 lines = s.split("\n")
 lines = [line for line in lines if line.strip()]
-PAIR = []
-for i in range(1, len(lines)-1, 17):
-    chunk = "\n".join(lines[i:i+17])
-    PAIR.append(chunk)
-#=========================================================
-#we need to make_dictionary with this data at another function
-'''
-cot_info = {}
-for i in PAIR:
-    make_dictionary(i,cot_info)
-'''
 #=========================================================
 #we creat text to see what we do and change our data
 f = open("AllData-first-edition.txt", "a")
 f.write(str(lines)+"\n""\n"+str(PAIR))
 f.close()
 #=========================================================
-# we collect the name of pairs to another text file
-f = open("Data-first-edition.txt", "a")
+# my index is pair 
+pairlist=[]
+f = open("pairlist.txt", "a")
 text = "Pair:" +"\n""\n"
 for i in range(6, len(lines)-1, 17):
     dash_index2 = lines[i].find("- ")
     key = lines[i][:dash_index2].strip()
-    text+= str(key)+"\n""\n"
+    text+= str(key)+"\n"
+    pairlist.append(key)
     
+f.write(text)
+f.close()
+#=========================================================
+# we collect positions number line for a pair
+positions=[]
+f = open("positions.txt", "a")
+text1 = "positions:" +"\n""\n"
+for i in range(9, len(lines)-1, 17):
+    dash_index2 = lines[i].find("- ")
+    key = lines[i][:dash_index2].strip()
+    key2= key.replace(',', '')
+    positions2 = key2.split()
+    #=========================
+    def safe_float_conversion(s):
+        try:
+            return float(s)
+        except ValueError:
+            return None
+    #===========================
+    # Convert and filter out None values
+    float_numbers = [safe_float_conversion(comp) for comp in positions2 if safe_float_conversion(comp) is not None]
+    text1+= str(float_numbers)+"\n"
+    positions.append(float_numbers)
+    
+f.write(text1)
+f.close()
+#=========================================================
+# we collect Changepositions number line for a pair
+Changepositions=[]
+f = open("Changepositions.txt", "a")
+text2 = "Changepositions:" +"\n""\n"
+for i in range(11, len(lines)-1, 17):
+    dash_index2 = lines[i].find("- ")
+    key = lines[i][:dash_index2].strip()
+    key2= key.replace(',', '')
+    components = key2.split()
+    #=========================
+    def safe_float_conversion(s):
+        try:
+            return float(s)
+        except ValueError:
+            return None
+    #===========================
+    # Convert and filter out None values
+    float_numbers = [safe_float_conversion(comp) for comp in components if safe_float_conversion(comp) is not None]
+    text2+= str(float_numbers)+"\n"
+    Changepositions.append(float_numbers)
+    
+f.write(text2)
+f.close()
+browser.quit()
+#=========================================================
+#======================DataFrames=========================
+#=========================================================
+topic = ['Long_Dealers','Short_Dealers','Spreading_Dealers',
+        'Long_AssetManager','Short_AssetManager','Spreading_AssetManager',
+        'Long_Leveraged','Short_Leveraged','Spreading_Leveraged',
+        'Long_Reportables','Short_Reportables','Spreading_Reportables',
+        'Long_Nonreportable','Short_Nonreportable']
+df = pd.DataFrame(data=Changepositions,index=pairlist,columns=topic)
+#=========================================================
+#======================Sell_Signals=========================
+sell = df[(df['Long_Leveraged'] < df['Short_Leveraged']) &
+            (df['Long_AssetManager'] < df['Short_AssetManager'])]
+sell = sell[['Long_AssetManager','Short_AssetManager','Long_Leveraged', 'Short_Leveraged']]
+# Convert DataFrame to string with index
+Selldata_str_with_index = sell.to_string(index=True)
+# Print & SaveText the string with index
+print(Selldata_str_with_index)
+selltext= "CFTC-Sell-filters"+"\n""\n"
+f = open("Sell.txt", "a")   
+f.write(Selldata_str_with_index)
+f.close()
+#=========================================================
+#======================Buy_Signals=========================
+buy = df[(df['Long_Leveraged'] > df['Short_Leveraged']) &
+            (df['Long_AssetManager'] > df['Short_AssetManager'])]
+buy = buy[['Long_AssetManager','Short_AssetManager','Long_Leveraged', 'Short_Leveraged']]
+# Convert DataFrame to string with index
+Buydata_str_with_index = buy.to_string(index=True)
+# Print & SaveText the string with index
+print(Buydata_str_with_index)
+buytext= "CFTC-Buy-filters"+"\n""\n"
+f = open("Buy.txt", "a")   
+f.write(Buydata_str_with_index)
+f.close()
+
 
 f.write(text)
 f.close()
